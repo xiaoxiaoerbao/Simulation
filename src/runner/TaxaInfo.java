3e760ffb7f65dcb0ca43cc11ed18a43e1f03274f
xiaoxiaoerbao <Xudaxing@2018>
2023-03-24 11:06:42 +0800

    simplifying object attributes

--- src/runner/TaxaInfo.java
+++ src/runner/TaxaInfo.java
@@ -1,56 +1,78 @@
 package runner;
 
-import com.google.common.collect.Table;
-import utils.RowTableTool;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
+import utils.IOTool;
+import utils.PStringUtils;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+/**
+ * Taxon	Population	PopulationName
+ * tsk_0	4	E
+ * tsk_1	4	E
+ * tsk_2	4	E
+ * tsk_3	4	E
+ * tsk_4	4	E
+ */
 public class TaxaInfo {
 
-    Table<String,String,String> taxon_Pop_PopName;
-    List<String> popName;
-    Map<String, String> taxaPopNameMap;
-    Map<String, List<String>> popTaxaListMap;
+    String[] taxa; // unique among all taxa
+    int[] populationID; // population ID in tskit
+    String[] populationName; // e.g., E, D, C
+
     public TaxaInfo(String taxaInfo){
-        this.taxon_Pop_PopName = RowTableTool.getTable(taxaInfo, 0 ,1,2);
-        this.popName = new ArrayList<>();
-        this.taxaPopNameMap = new HashMap<>();
-        for (Table.Cell<String,String,String> cell : this.taxon_Pop_PopName.cellSet()){
-            this.taxaPopNameMap.put(cell.getRowKey(), cell.getValue());
-            this.popName.add(cell.getValue());
-        }
-        this.popTaxaListMap = new HashMap<>();
-        for (String pop : this.popName){
-            this.popTaxaListMap.put(pop, new ArrayList<>());
-        }
-        for (Table.Cell<String,String,String> cell : this.taxon_Pop_PopName.cellSet()){
-            this.popTaxaListMap.get(cell.getValue()).add(cell.getRowKey());
-        }
-    }
 
-    public List<String> getPopName() {
-        return popName;
-    }
-
-    public Map<String, List<String>> getPopTaxaListMap() {
-        return popTaxaListMap;
-    }
-
-    public Map<String, String> getTaxaPopNameMap() {
-        return taxaPopNameMap;
+        List<String> taxaList = new ArrayList<>();
+        IntList populationIDList = new IntArrayList();
+        List<String> populationNameList = new ArrayList<>();
+        try (BufferedReader br = IOTool.getReader(taxaInfo)) {
+            String line;
+            List<String> temp;
+            br.readLine();
+            while ((line=br.readLine())!=null){
+                temp = PStringUtils.fastSplit(line);
+                taxaList.add(temp.get(0));
+                populationIDList.add(Integer.parseInt(temp.get(1)));
+                populationNameList.add(temp.get(2));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.taxa = taxaList.toArray(new String[0]);
+        this.populationID = populationIDList.toIntArray();
+        this.populationName = populationNameList.toArray(new String[0]);
     }
 
     public int getPopSampleSize(String popName){
-        return this.getPopTaxaListMap().get(popName).size();
+//        return this.getPopTaxaListMap().get(popName).size();
+        int count = 0;
+        for (String populationName : this.populationName){
+            if (populationName.equals(popName)){
+                count++;
+            }
+        }
+        return count;
     }
 
     public List<String> getTaxaListOf(String popName){
-        return this.popTaxaListMap.get(popName);
+        List<String> taxaList = new ArrayList<>();
+        for (int i = 0; i < this.populationName.length; i++) {
+            if (this.populationName[i].equals(popName)){
+                taxaList.add(this.taxa[i]);
+            }
+        }
+        return taxaList;
     }
 
     public String getTaxonPop(String taxon){
-        return this.taxaPopNameMap.get(taxon);
+        for (int i = 0; i < this.taxa.length; i++) {
+            if (this.taxa[i].equals(taxon)){
+                return this.populationName[i];
+            }
+        }
+        return null;
     }
 }
